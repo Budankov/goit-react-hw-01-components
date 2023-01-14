489452ff2d7e110d5d6e1a7f1f23a8f3a4d70135
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-14 13:00:58 +0200

    Update task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import PropTypes from 'prop-types';
+import { Profile } from './Profile/Profile';
+import user from './data/user';
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
